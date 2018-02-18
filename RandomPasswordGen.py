5e3772d37fe4df41c9f5c703feb586dfb9a8325a
Adith <adithdinesh97@gmail.com>
2018-02-18 19:32:07 +0530

    Added comments to Password Generator

--- RandomPasswordGen.py
+++ RandomPasswordGen.py
@@ -2,11 +2,13 @@ import secrets
 import random
 import string
 
+    #OBTAIN A SET OF ALL CHARACTERS AND DIGITS
 allchars = string.ascii_letters+string.digits
 
 
 while True:
     password = ''.join(secrets.choice(allchars) for i in range(10))
+    # CHECKS CONDITIONS THAT NEED TO BE SATISFIED
     if (any(c.islower() for c in password)
         and any(c.isupper() for c in password)
         and sum(c.isdigit() for c in password) >= 2):
