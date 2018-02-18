dc8dbf896b2262141c7bf6f9f5e1e88f8dfddc40
Adith <adithdinesh97@gmail.com>
2018-02-18 11:05:34 +0530

    Added DeleteImages Python Script

---
+++ DeleteImages.py
@@ -0,0 +1,36 @@
+import time
+import os
+from sys import argv
+script_name = argv
+choice=input("Do you want to validate each delete operation? ")
+print("EXIT AT ANYTIME USING exit")
+
+if(choice == "yes"):
+    for i in os.listdir():
+        if i.endswith(".jpg"):
+            confirm=input("Do you want to delete "+i+" ?\n")
+            if(confirm =="yes"):
+                os.remove(i)
+                print(i+" was successfully removed\n ")
+            elif(confirm =="no"):
+                print(i+" will not be deleted\n ")
+            elif(confirm =="exit"):
+                quit()
+elif(choice == "no"):
+    for i in os.listdir():
+        if i.endswith(".jpg"):
+            os.remove(i)
+            print("\n"+i+" was successfully removed")
+elif(choice == "exit"):
+    quit()
+
+count=0
+
+for i in os.listdir():
+    if i.endswith(".jpg"):
+        count+=1
+if(count == 0):
+    print("No more files remaining!")
+else:
+    print(count,"images remaining in this folder")
+time.sleep(5)
